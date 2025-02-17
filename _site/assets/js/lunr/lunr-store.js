b48e02a626efe94cb6972075668d7204e7903a3a
RobBEN93 <robnavar84@gmail.com>
2025-02-16 18:04:50 -0600

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"Welcome   Hello world, this is a post test.  ","categories": [],
+        "tags": [],
+        "url": "/post_test/",
+        "teaser": null
+      },{
+    "title": "Welcome!",
+    "excerpt":"Welcome   Hello world, this is a post test.  ","url": "http://localhost:4000/_posts/2025-02-16-post_test/"
+  },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist…  ","url": "http://localhost:4000/404.html"
+  }]
