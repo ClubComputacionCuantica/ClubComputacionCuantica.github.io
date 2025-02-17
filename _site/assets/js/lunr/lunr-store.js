75edc8d008bc59bbf431df2db8dfcdeab788952a
RobBEN94 <robnavar84@gmail.com>
2025-02-17 13:36:13 -0600

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
   },{
     "title": "Page Not Found",
     "excerpt":"Sorry, but the page you were trying to view does not exist…  ","url": "http://localhost:4000/404.html"
+  },{
+    "title": "Talleres",
+    "excerpt":"Sample document listing for the collection _talleres.  ","url": "http://localhost:4000/Talleres/"
+  },{
+    "title": "Física Emergente: Programación Cuántica y Física de Frontera",
+    "excerpt":"# Física Emergente: Programación Cuántica y Física de Frontera ## 1. Introducción a la Computación Cuántica ### 1.1 ¿Qué es un qubit? ¿Qué es una computadora cuántica? Exploraremos los conceptos fundamentales de la computación cuántica, comenzando con el qubit y cómo se diferencia de los bits clásicos, así como las...","url": "http://localhost:4000/temario/"
   }]
